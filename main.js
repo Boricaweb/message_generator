b74d3120a3a8daca022dae0f5510615255fd223c
Boricaweb <thanapolsrangkaew@gmail.com>
2025-09-02 18:56:37 +0700

    2nd commit start write predict message function

---
+++ main.js
@@ -0,0 +1,13 @@
+const activitiesFactory = () => {
+    return {
+        verb: ['play', 'go', 'watch', 'learn']
+    }
+}
+
+const predictMessage = (username) => {
+    if (typeof username === 'string') {
+        
+    } else {
+        return 'incorrect name';
+    }
+}
