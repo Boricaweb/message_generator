b9d6a4d42ebfb3a32bf045a1c275e62a4b9c9dda
Boricaweb <thanapolsrangkaew@gmail.com>
2025-09-11 17:22:42 +0700

    3rt commit created a fetch variable

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const activitiesFactory = () => {
-    return {
-        verb: ['play', 'go', 'watch', 'learn']
+const randomGame = async () => {
+    try {
+        const response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=games&type=uppercase&words=1000");
+        const data = await response.json();
+        const game = data[Math.floor(Math.random()*999)]["word"];
+        return game;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
     }
 }
 
-const predictMessage = (username) => {
-    if (typeof username === 'string') {
-        
-    } else {
-        return 'incorrect name';
-    }
-}
+
+
+
+
+
