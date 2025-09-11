26371a88a955ef2a031709dc57d2e90ad36fb3cd
Boricaweb <thanapolsrangkaew@gmail.com>
2025-09-11 18:50:55 +0700

    5th commit finished debugging main function, trying to do input on node task

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
 //This is the program create for someone who can't make a decision, what game they would like to play. Alright, enjoy your game!
 
-//Random game name by API from the Random Words API website
-//Use async and await for reduce the execute time
-
-
-async function randomGame() {
-    try {
-        const response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=games&type=uppercase&words=100");
-        const data = await response.json();
-        const game = data[Math.floor(Math.random()*99)]["word"];
-        return game;
-    } catch (error) {
-        /*catch the error which may come from website*/
-        console.error('Error:', error);
-        return null;
-    }
-}
-
-//Put the function into variable
-const yourGame = randomGame();
-
 //Function factory for random message
-function iWillPlay(name) {
+function iWillPlay(name, game) {
     return {
         _name: name, //Your name
+        _game: game, //Your game
         get message() {
             //Game message method
-            if (typeof this._name === 'string') {
-                return this._name + ' will play ' + yourGame;
+            if (typeof this._name === 'string' && typeof this._game === 'string') {
+                console.log(`${this._name} will play ${this._game}`);
             } else {
-                return 'Please input your name in string';
+                console.log('Please input your name in string');
             }
         }
     }
 }
 
-const randomMessage = iWillPlay().message;
+//Random game name by API from the Random Words API website
+//Use async and await for reduce the execute time
+randomGame();
 
-console.log(randomMessage);
+async function randomGame() {
+    try {
+        const response = await fetch("https://random-words-api.kushcreates.com/api?language=en&category=games&type=uppercase&words=100");
+        if (!response.ok) {
+            throw new Error('Could not fetch resource')
+        }
+        const data = await response.json();
+        const game = data[Math.floor(Math.random()*data.length)].word;
+        iWillPlay('Non', game).message;
+    } catch (error) {
+        /*catch the error which may come from website*/
+        console.error(error);
+        return null;
+    }
+}
+
 
 
 
